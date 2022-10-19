afa8daf16c6c6cb514695c1bc726399d2e34d9e6
tatasulysta <tatasulystaaa@gmail.com>
2022-10-19 23:13:38 +0700

    remove bracket

--- src/components/CardPopular/index.jsx
+++ src/components/CardPopular/index.jsx
@@ -5,11 +5,11 @@ function CardPopular(props) {
   return (
     <div className={styles.card}>
       {!props.isReversed && <img src={props.img} alt="" />}
-      <div className={'container d-flex flex-column ps-3'}>
-        <h2 className={'mt-3 mb-2'}>{props.category}</h2>
-        <h1 className={'w-75 mb-2'}>{props.title}</h1>
-        <p className={'w-100 '}>{props.description}</p>
-        <div className={'align-self-end mb-3'}>
+      <div className="container d-flex flex-column ps-3">
+        <h2 className="mt-3 mb-2">{props.category}</h2>
+        <h1 className="w-75 mb-2">{props.title}</h1>
+        <p className="w-100">{props.description}</p>
+        <div className="align-self-end mb-3">
           <ButtonSave>Save</ButtonSave>
         </div>
       </div>
--- src/components/LayoutSectionPopular/index.jsx
+++ src/components/LayoutSectionPopular/index.jsx
@@ -3,7 +3,7 @@ import styles from './style.module.css';
 function LayoutSectionPopular(props) {
   return (
     <section className={`${styles.layout} container mb-5 `}>
-      <h1 className={` mb-3 mt-4`}>Popular News</h1>
+      <h1 className="mb-3 mt-4">Popular News</h1>
       <div
         className={`d-flex flex-row gap-5 align-items-center justify-content-between ${styles.cards}`}
       >
