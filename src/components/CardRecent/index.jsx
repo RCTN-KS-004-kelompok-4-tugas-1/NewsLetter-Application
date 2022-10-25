32f1bf772afd03125a0104661d89a2e5a1f513a3
AfiraRolobessy03 <81508591+AfiraRolobessy03@users.noreply.github.com>
2022-10-25 19:00:47 +0700

    coba

--- src/components/CardRecent/index.jsx
+++ src/components/CardRecent/index.jsx
@@ -6,16 +6,13 @@ import { IconBookmark, IconBookmarkFilled } from '../Icons';
 function CardRecent(props) {
   return (
     <div className={styles.card}>
-    
       {!props.isReversed && <img src={props.img} alt="" />}
       <div className="card-bodyz">
-      {/* <div class="d-flex flex-row gap-5 align-items-center justify-content-between style_cards_aTBwN">
-      <div class="ntainer d-coflex flex-column ps-3"> */}
         <h2 className="card-title">{props.category}</h2>
         <h3 className="card-title">{props.title}</h3>
         <p className="card-text">{props.description}</p>
         <h6 className="card-header d-flex justify-content-between">
-          {props.author} 
+          {props.author}
           <IconButton>
             {props.active ? (
               <IconBookmark color="#ffffff" />
@@ -28,8 +25,6 @@ function CardRecent(props) {
       {props.isReversed && (
         <img className={styles.imgReversed} src={props.img} alt="" />
       )}
-    {/* </div>
-    </div> */}
     </div>
   );
 }
